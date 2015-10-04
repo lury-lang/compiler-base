efcf3c47ff5aa2f8c2cbd659fd8f0b4f695ecbdb
Tomona Nanase <nanase@users.noreply.github.com>
2015-10-05 00:43:44 +0900

    Add unit tests

--- UnitTest/OutputLoggerTest.cs
+++ UnitTest/OutputLoggerTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Lury.Compiling.Logger;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -61,6 +62,31 @@ namespace UnitTest
 
             logger.Clear();
             Assert.AreEqual(0, logger.Outputs.Count());
+        }
+
+        [TestMethod]
+        public void CopyToTest()
+        {
+            OutputLogger logger1 = new OutputLogger();
+            logger1.ReportInfo(number);
+            logger1.ReportWarn(number);
+            logger1.ReportError(number);
+
+            OutputLogger logger2 = new OutputLogger();
+            logger1.CopyTo(logger2);
+            
+            Assert.AreEqual(3, logger2.Outputs.Count());
+            Assert.AreEqual(1, logger2.InfoOutputs.Count());
+            Assert.AreEqual(1, logger2.WarnOutputs.Count());
+            Assert.AreEqual(1, logger2.ErrorOutputs.Count());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CopyToErrorTest()
+        {
+            OutputLogger logger = new OutputLogger();
+            logger.CopyTo(null);
         }
     }
 }
